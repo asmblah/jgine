12b6d73de6e50e7f11c85668ab243f50993d000f
Daniel Phillimore <dan@ovms.co>
2012-05-16 17:14:27 +0100

    Added demo page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>jGine demo</title>
+
+        <script data-main="js/main" type="text/javascript" src="lib/require-jquery.js"></script>
+    </head>
+    <body>
+        <h1>jGine demo</h1>
+    </body>
+</html>
