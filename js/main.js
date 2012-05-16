2e797e31ed2bed90092520cabe90fa645e7bfc7e
Daniel Phillimore <dan@ovms.co>
2012-05-16 17:13:55 +0100

    Added demo routines

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*
+ *  Main load routines for jGine demo
+ */
+require({
+    paths: {
+        "js": "/jgine/lib/jgine/js",
+        "lib": "/jgine/lib"
+    }
+}, [
+    "js/util",
+    "js/classes/File/AliasWavefront",
+    "js/classes/World"
+], function(
+    util,
+    AliasWavefrontFile,
+    World
+) {
+    var aliasWavefrontFile = new AliasWavefrontFile("models/car.obj");
+
+    aliasWavefrontFile.read()
+        .done(function(objects) {
+            var world = new World(objects);
+
+            world.render();
+        })
+        .fail(function() {
+            alert("Could not load file");
+        });
+});
