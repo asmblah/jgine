d82d3d77e07abadb15b9c271d185a84ed5d71123
Daniel Phillimore <dan@ovms.co>
2012-05-17 21:52:46 +0100

    Added preliminary WebGL rendering support to demo

--- js/main.js
+++ js/main.js
@@ -8,23 +8,79 @@ require({
     }
 }, [
     "js/util",
+    "js/classes/Promise",
+    "js/classes/WebGL",
+    "js/classes/WebGL/Program",
+    "js/classes/WebGL/Shader/Vertex",
+    "js/classes/WebGL/Shader/Fragment",
     "js/classes/File/AliasWavefront",
     "js/classes/World"
-], function(
+], function (
     util,
+    Promise,
+    WebGL,
+    WebGLProgram,
+    WebGLVertexShader,
+    WebGLFragmentShader,
     AliasWavefrontFile,
     World
 ) { "use strict";
 
-    var aliasWavefrontFile = new AliasWavefrontFile("models/car.obj");
+    function getScript(id) {
+        return document.getElementById(id).textContent;
+    }
 
-    aliasWavefrontFile.read()
-        .done(function(objectGroup) {
-            var world = new World(objectGroup);
+    function initWebGL() {
+        var promise = new Promise(webGL);
 
-            world.render();
+        webGL.init()
+            .done(function () {
+                webGLVertexShader.compile()
+                    .done(function () {
+                        webGLFragmentShader.compile()
+                            .done(function () {
+                                webGLProgram
+                                    .attach(webGLVertexShader)
+                                    .attach(webGLFragmentShader)
+                                    .link()
+                                        .done(function () {
+                                            promise.resolve();
+                                        });
+                            })
+                            .fail(function () {
+                                promise.reject();
+                            });
+                    })
+                    .fail(function () {
+                        promise.reject();
+                    });
+            }).fail(function () {
+                promise.reject();
+            });
+
+        return promise;
+    }
+
+    var canvas = document.getElementById("canvas"),
+        webGL = new WebGL(canvas),
+        webGLProgram = new WebGLProgram(webGL),
+        webGLVertexShader = new WebGLVertexShader(webGL, getScript("vertexShader")),
+        webGLFragmentShader = new WebGLFragmentShader(webGL, getScript("fragmentShader")),
+        aliasWavefrontFile = new AliasWavefrontFile("models/car.obj");
+
+    initWebGL()
+        .done(function () {
+            aliasWavefrontFile.read()
+                .done(function (objectGroup) {
+                    var world = new World(objectGroup);
+
+                    world.render();
+                })
+                .fail(function (error) {
+                    alert("Could not load file - " + error);
+                });
         })
-        .fail(function(error) {
-            alert("Could not load file - " + error);
+        .fail(function (error) {
+            throw new Error("Failed to init WebGL - " + error);
         });
 });
