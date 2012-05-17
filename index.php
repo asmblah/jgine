4f28912696869403f0191590d817b8dd02ec35d7
Daniel Phillimore <dan@ovms.co>
2012-05-17 21:53:13 +0100

    Added <canvas> & shaders to demo

--- index.php
+++ index.php
@@ -3,9 +3,50 @@
     <head>
         <title>jGine demo</title>
 
+        <script id="vertexShader" type="x-shader/x-vertex">
+            attribute vec3 aVertexPosition;
+
+            uniform mat4 uMVMatrix;
+            uniform mat4 uPMatrix;
+
+            void main(void) {
+                gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+            }
+        </script>
+
+        <script id="fragmentShader" type="x-shader/x-fragment">
+            precision mediump float;
+
+            void main(void) {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            }
+        </script>
+
         <script data-main="js/main" type="text/javascript" src="lib/require-jquery.js"></script>
+
+        <style type="text/css">
+            html, body {
+                margin: 0; padding: 0; outline: 0;
+
+                width: 100%; height: 100%;
+            }
+            body {
+                background: black;
+            }
+            #canvas {
+                border: solid white 1px;
+
+                display: block;
+
+                margin: auto;
+            }
+        </style>
     </head>
     <body>
         <h1>jGine demo</h1>
+
+        <canvas id="canvas" width="500" height="500">
+            <p>Sorry, your browser doesn't support HTML5.</p>
+        </canvas>
     </body>
 </html>
